790fdc58b99d3dd3aa97a9d44d2c018103aa8794
Amoke Emmanuel <emmanuelamoke35@gmail.com>
2025-10-31 02:57:22 +0100

    version 0.01

--- src/app/(prepage)/forgot-password/page.jsx
+++ src/app/(prepage)/forgot-password/page.jsx
@@ -2,3 +2,9 @@ export const metadata = {
     title: "Reset Password | Secure Your Trading Journal Access",
     description: "Forgot your password? No problem. Enter your email to reset your credentials and regain secure access to your trading journal and account data"
 }
+
+export default function ForgotPasswordPage() {
+    return (
+        <></>
+    )
+}
